b80299089c803161ab7a863b2736bc54bf898ec4
GandhiHarsh2003 <23gandhiharsh@gmail.com>
2023-09-16 16:00:04 -0400

    new changes

--- frontend/src/CalculatorPage.js
+++ frontend/src/CalculatorPage.js
@@ -1,85 +1,177 @@
-// pages/index.js
-
-import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-
-const athena_backend_url = process.env.REACT_APP_BACKEND_URL;
+import React, { useState, useEffect } from 'react';
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  Cell,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip as BarTooltip,
+  Legend,
+  ResponsiveContainer,
+  RadarChart,
+  Radar,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+} from 'recharts';
 
 const CalculatorPage = () => {
-    const [carbonFields, setCarbonFields] = useState([
-        { name: 'Tomato', imagePath: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Tomato_je.jpg', carbonCost: 1.99, quantity: 1 },
-        { name: 'Potato', imagePath: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/2560px-Patates.jpg', carbonCost: 0.99, quantity: 1 },
-        { name: 'Banana', imagePath: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Bananas_white_background_DS.jpg/2560px-Bananas_white_background_DS.jpg', carbonCost: 0.49, quantity: 1 },
-    ]);
+  const [carbonFields, setCarbonFields] = useState([
+    {
+      name: 'Tomato',
+      imagePath: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Tomato_je.jpg',
+      carbonCost: 1.99,
+      quantity: 1,
+    },
+    {
+      name: 'Potato',
+      imagePath: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/2560px-Patates.jpg',
+      carbonCost: 0.99,
+      quantity: 1,
+    },
+    {
+      name: 'Banana',
+      imagePath: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Bananas_white_background_DS.jpg/2560px-Bananas_white_background_DS.jpg',
+      carbonCost: 0.49,
+      quantity: 1,
+    },
+    {
+      name: 'Onion',
+      imagePath: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Bananas_white_background_DS.jpg/2560px-Bananas_white_background_DS.jpg',
+      carbonCost: 0.49,
+      quantity: 1,
+    },
+    {
+      name: 'Coriander',
+      imagePath: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Bananas_white_background_DS.jpg/2560px-Bananas_white_background_DS.jpg',
+      carbonCost: 0.49,
+      quantity: 1,
+    },
+  ]);
 
-//   const fetchCameras = async () => {
-//     const response = await axios.get(
-//       `${athena_backend_url}/api/cameras`
-//     );
-//     const cameraObject = response.data.reduce((acc, cur) => {
-//       const { cameraId, ...rest } = cur;
-//       acc[cameraId] = rest;
-//       return acc;
-//     }, {});
-//     setCameras(cameraObject);
-//   };
+  const [total, setTotal] = useState(0);
 
-//   useEffect(() => {
-//     fetchCameras();
-//   }, []);
+  useEffect(() => {
+    const newTotal = carbonFields.reduce((acc, item) => acc + item.carbonCost * item.quantity, 0);
+    setTotal(newTotal);
+  }, [carbonFields]);
 
+  const handleQuantityChange = (index, newValue) => {
+    if (!isNaN(newValue) && newValue >= 0) {
+      const intValue = Math.floor(Number(newValue));
+      const updatedCarbonFields = [...carbonFields];
+      updatedCarbonFields[index].quantity = intValue;
+      setCarbonFields(updatedCarbonFields);
+    }
+  };
 
-//   const fetchImages = async (date, startTime, endTime) => {
-//     if (!date || !startTime || !endTime) {
-//       date = '2023-05-03';
-//       startTime = '15:45';
-//       endTime = '16:45';
-//     }
-    
-//     startTime = startTime.replace(':', '-');
-//     startTime = startTime.concat('-00');
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-//     endTime = endTime.replace(':', '-');
-//     endTime = endTime.concat('-00');
+  const pieChartData = carbonFields.map((carbonField) => ({
+    name: carbonField.name,
+    value: carbonField.carbonCost * carbonField.quantity,
+  }));
 
+  const barChartData = carbonFields.map((carbonField) => ({
+    name: carbonField.name,
+    uv: carbonField.carbonCost * carbonField.quantity,
+  }));
 
-//     const start_date = date + '-' + startTime;
-//     const end_date = date + '-' + endTime;
-
-//     const response = await axios.get(
-//       // `${athena_backend_url}/api/images/date_range?start_date=2023-04-01-09-00-00&end_date=2023-06-01-09-02-00`
-//       `${athena_backend_url}/api/images/date_range?start_date=${start_date}&end_date=${end_date}`
-//     );
-//     setImagesObjects(response.data);
-//   };
-
-
-//   useEffect(() => {
-//     fetchImages();
-//   }, []);
+  const radarChartData = carbonFields.map((carbonField) => ({
+    subject: carbonField.name,
+    A: carbonField.carbonCost * carbonField.quantity,
+    fullMark: 3.0,
+  }));
 
   return (
     <main className="h-screen flex">
-      <div className="flex flex-col flex-grow pr-4 items-center mt-4">
-        <h1 className="text-2xl font-semibold mb-4">Carbon Emission Calculator</h1>
-        <div className="overflow-y-scroll">
-            {
-                carbonFields.map((carbonField, index) => (
-                    <div key={index} className="flex flex-row items-center mb-4">
-                        <img className="w-16 h-16 mr-4" src={carbonField.imagePath} alt={carbonField.name} />
-                        <div className="flex flex-col">
-                            <div className="flex flex-row items-center">
-                                <h2 className="text-xl font-semibold mr-4">{carbonField.name}</h2>
-                                <h2 className="text-xl font-semibold mr-4">{carbonField.carbonCost}</h2>
-                            </div>
-                            <div className="flex flex-row items-center">
-                                <h2 className="text-xl font-semibold mr-4">Quantity: {carbonField.quantity}</h2>
-                            </div>
-                        </div>
-                    </div>
-                ))
-
-            }
+      <div className="w-1/2 p-8 overflow-y-auto">
+        <h1 className="text-3xl font-semibold mb-4">Carbon Emission Calculator</h1>
+        <div>
+          {carbonFields.map((carbonField, index) => (
+            <div key={index} className="flex flex-row items-center mb-4">
+              <img className="w-16 h-16 mr-4 rounded-lg" src={carbonField.imagePath} alt={carbonField.name} />
+              <div className="flex flex-col">
+                <div className="flex flex-row items-center">
+                  <h2 className="text-lg font-semibold mr-4">{carbonField.name}</h2>
+                  <h2 className="text-lg font-semibold">${carbonField.carbonCost.toFixed(2)}</h2>
+                </div>
+                <div className="flex flex-row items-center">
+                  <input
+                    type="number"
+                    id={`quantityInput-${index}`}
+                    placeholder="Quantity"
+                    value={carbonField.quantity}
+                    onChange={(e) => handleQuantityChange(index, e.target.value)}
+                    className="w-16 p-2 border rounded-lg"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-lg font-semibold mt-4">Total: ${total.toFixed(2)}</h2>
+        </div>
+      </div>
+      <div className="w-1/2 p-8">
+        <div className="mb-8">
+          <h2 className="text-xl font-semibold mb-4">Carbon Emission Breakdown</h2>
+          <ResponsiveContainer width="100%" height={300}>
+            <PieChart>
+              <Pie
+                data={pieChartData}
+                cx="50%"
+                cy="50%"
+                innerRadius={60}
+                outerRadius={80}
+                fill="#8884d8"
+                paddingAngle={5}
+                dataKey="value"
+              >
+                {pieChartData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="mb-8">
+          <h2 className="text-xl font-semibold mb-4">Carbon Emission by Item</h2>
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart
+              data={barChartData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Bar dataKey="uv" fill="#8884d8" />
+              <Tooltip />
+              <Legend />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-4">Carbon Emission Radar Chart</h2>
+          <ResponsiveContainer width="100%" height={300}>
+            <RadarChart outerRadius="80%" data={radarChartData}>
+              <PolarGrid />
+              <PolarAngleAxis dataKey="subject" />
+              <PolarRadiusAxis />
+              <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            </RadarChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </main>
